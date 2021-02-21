cb4dad81b6ed309f39b0c622f30c39802b6bde87
Miriam Neuenfeld <miriam.franke77@gmail.com>
2021-02-21 17:46:29 +0100

    added authorization for editing user

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/tweets', [App\Http\Controllers\TweetController::class, 'index'])->name('home');
     Route::post('/tweets', [App\Http\Controllers\TweetController::class, 'store']);
 
-    Route::get('/profiles/{user:name}/follow', [App\Http\Controllers\FollowsController::class, 'store']);
+    Route::post('/profiles/{user:name}/follow', [App\Http\Controllers\FollowsController::class, 'store']);
+    Route::get(
+        '/profiles/{user:name}/edit',
+        [App\Http\Controllers\ProfilesController::class,
+            'edit']
+    )->middleware('can:edit,user');
 });
 
 Route::get('/profiles/{user:name}', [App\Http\Controllers\ProfilesController::class, 'show'])->name('profile');
